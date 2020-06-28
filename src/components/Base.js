b15d493e76351484148695a007b0a029e815c413
Kush Daga <kushdaga@hotmail.com>
2020-06-28 21:53:15 +0530

    Initialized repo

---
+++ src/components/Base.js
@@ -0,0 +1,89 @@
+/** @format */
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+const Base = ({ addBase, pizza }) => {
+	const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
+
+	const containerVariants = {
+		hidden: {
+			opacity: 0,
+			x: "100vw",
+		},
+		visible: {
+			opacity: 1,
+			x: 0,
+			transition: {
+				type: "spring",
+				delay: 0.5,
+			},
+		},
+		exit: {
+			x: "-100vw",
+			transition: { ease: "easeInOut" },
+		},
+	};
+	const nextVariants = {
+		hidden: {
+			x: "-100vw",
+		},
+		visible: {
+			x: 0,
+			transition: { type: "spring", stiffness: 120 },
+		},
+	};
+
+	const buttonVariants = {
+		hover: {
+			scale: 1.1,
+			textShadow: "0px 0px 8px rgb(255,255,255)",
+			boxShadow: "0px 0px 8px rgb(255,255,255)",
+			transition: {
+				duration: 0.3,
+				yoyo: Infinity,
+			},
+		},
+	};
+
+	return (
+		<motion.div
+			variants={containerVariants}
+			initial="hidden"
+			animate="visible"
+			exit="exit"
+			className="base container">
+			<h3>Step 1: Choose Your Base</h3>
+			<ul>
+				{bases.map((base) => {
+					let spanClass = pizza.base === base ? "active" : "";
+					return (
+						<motion.li
+							whileHover={{
+								scale: 1.3,
+								color: "#f8e112",
+								originX: 0,
+							}}
+							transition={{ type: "spring", stiffness: 300 }}
+							key={base}
+							onClick={() => addBase(base)}>
+							<span className={spanClass}>{base}</span>
+						</motion.li>
+					);
+				})}
+			</ul>
+
+			{pizza.base && (
+				<motion.div className="next" variants={nextVariants}>
+					<Link to="/toppings">
+						<motion.button variants={buttonVariants} whileHover="hover">
+							Next
+						</motion.button>
+					</Link>
+				</motion.div>
+			)}
+		</motion.div>
+	);
+};
+
+export default Base;
